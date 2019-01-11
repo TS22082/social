763a93ac511dc5207b8492fbb0c092c8e8d58002
GeekCentric <ts22082@gmail.com>
2019-01-11 14:55:11 -0800

    redux session handling for logged in users

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from './../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 // Register user
 export const registerUser = (userData, history) => dispatch => {
@@ -15,4 +17,33 @@ export const registerUser = (userData, history) => dispatch => {
 };
 
 // Login - Get user token
-export const loginUser = userDate => dispatch => {};
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      //save to local storage
+      const { token } = res.data;
+      // set token to localstorage
+      localStorage.setItem('jwtToken', token);
+      // set token to auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set Logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
