40f7b48464a02820fe9f349249503c4a9326b1ba
GeekCentric <ts22082@gmail.com>
2019-01-22 12:48:19 -0800

    make not-found component and redirect if needed

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import AddEducation from './components/add-credentials/AddEducation'
 import './App.css'
 import Profiles from './components/profiles/Profiles'
 import Profile from './components/profile/Profile'
+import NotFound from './components/not-found/NotFound'
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -89,6 +90,7 @@ class App extends Component {
                   component={AddEducation}
                 />
               </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
