575f9a9576542dcf390a678ec12b370947f3a12e
GeekCentric <ts22082@gmail.com>
2019-01-22 09:46:29 -0800

    make profile header component and populate with job, name, place, social media links, and profile pic

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -13,7 +13,7 @@ import {
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading())
   axios
-    .get('api/profile')
+    .get('/api/profile')
     .then(res => {
       dispatch({
         type: GET_PROFILE,
@@ -32,7 +32,7 @@ export const getCurrentProfile = () => dispatch => {
 export const getProfileByHandle = handle => dispatch => {
   dispatch(setProfileLoading())
   axios
-    .get(`api/profile/handle/${handle}`)
+    .get(`/api/profile/handle/${handle}`)
     .then(res => {
       dispatch({
         type: GET_PROFILE,
