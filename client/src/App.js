47a3303d2e353a28193b9cedac344415d0fb8b82
GeekCentric <ts22082@gmail.com>
2019-01-24 10:14:16 -0800

    create add post compoennt and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import store from './store'
 
 import PrivateRoute from './components/common/PrivateRoute'
 
+import './App.css'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
@@ -20,10 +21,10 @@ import CreateProfile from './components/create-profile/CreateProfile'
 import EditProfile from './components/edit-profile/EditProfile'
 import AddExperience from './components/add-credentials/AddExperience'
 import AddEducation from './components/add-credentials/AddEducation'
-import './App.css'
 import Profiles from './components/profiles/Profiles'
 import Profile from './components/profile/Profile'
 import NotFound from './components/not-found/NotFound'
+import Posts from './components/posts/Posts'
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -89,6 +90,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
