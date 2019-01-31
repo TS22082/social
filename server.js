c6ba22728ce9fd0bcacc76b8b0430c6dc1fd6d67
GeekCentric <ts22082@gmail.com>
2019-01-31 10:10:58 -0800

    prepare for heroku production

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 const bodyParser = require('body-parser')
 const passport = require('passport')
-
+const path = require('path')
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
@@ -35,5 +35,14 @@ app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', index.html))
+  })
+}
+
 const port = process.env.port || 5000
-app.listen(port, console.log(`Server running on port: ${port}`))
+app.listen(port, () => console.log(`Server running on port: ${port}`))
