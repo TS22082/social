e5be255eb65479225bd1ffcda84f885c1e004fbf
GeekCentric <ts22082@gmail.com>
2019-01-31 17:32:42 -0800

    edit description

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ app.use(bodyParser.json())
 const db = require('./config/keys').mongoURI
 //Connect to mongodb
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log('Mongodb connected'))
   .catch(err => console.log(err))
 
@@ -39,7 +36,6 @@ app.use('/api/posts', posts)
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'))
-
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
